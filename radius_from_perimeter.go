26fa4f67e108b64b0a0126e4afba804ed6f689de
Dmitry Makarov <vensder@gmail.com>
2016-12-11 16:01:28 +0300

    radius_from_perimeter.go - added perimeter

--- radius_from_perimeter.go
+++ radius_from_perimeter.go
@@ -1,14 +1,21 @@
 package main
 
 import "math"
+import "fmt"
 
-func R(P float64) float64 {
-	return (P / (2 * math.Pi))
+func Radius(Perimeter float64) float64 {
+	return (Perimeter / (2 * math.Pi))
+}
+
+func Perimeter(Radius float64) float64 {
+	return (2 * math.Pi * Radius)
 }
 
 //Perimeter (P) = 2 · π · R
 //R = P/(2π)
 
 func main() {
-	println(R(10))
+
+	fmt.Printf("If Perimeter = 10, than Padius = %v\n", Radius(10))
+	fmt.Printf("If Radius = 1, than Perimeter = %v\n", Perimeter(1))
 }
