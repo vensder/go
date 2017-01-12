6eeb155d2d3fb8827c8c34e9bbaca4b24cfb42a1
vensder <vensder@gmail.com>
2017-01-12 23:42:56 +0300

    if_and_else.go

--- if_and_else.go
+++ if_and_else.go
@@ -9,15 +9,13 @@ func pow(x, n, lim float64) float64 {
 	if v := math.Pow(x, n); v < lim {
 		return v
 	} else {
-		fmt.Printf("%g >= %g\n", v, lim)
+		// fmt.Printf("%g >= %g\n", v, lim)
+		// can't use v here, though
+		return lim
 	}
-	// can't use v here, though
-	return lim
 }
 
 func main() {
-	fmt.Println(
-		pow(3, 2, 10),
-		pow(3, 3, 27),
-	)
+	fmt.Println(pow(3, 2, 10))
+	fmt.Println(pow(3, 3, 27))
 }
