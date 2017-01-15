46cb0ef6d7f7437a01daabf32e6c701c88fb16e8
vensder <vensder@gmail.com>
2017-01-15 15:50:38 +0300

    lasst

--- forever_func.go
+++ forever_func.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 	"strings"
+    "math"
 )
 
 func line(n int) {
 	symbol := "-"
 	fmt.Println(strings.Repeat(symbol, n))
+}
+
+func cos_int(n int) int {
+    math.Cos(float64(n))
 }
 
 func main() {
