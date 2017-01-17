dc5e4f9e05c456bd0d954ab1b013ccb98df309d9
vensder <vensder@gmail.com>
2017-01-18 02:21:06 +0300

    reverse.go

--- reverse.go
+++ reverse.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
 func reverse(s string) string {
 	r := []rune(s)
 	last_i := len(r) - 1
-
 	for i := 0; ; i++ {
 		r[i], r[last_i-i] = r[last_i-i], r[i]
 		if i >= (last_i - i) {
 			break
 		}
 	}
-
 	return string(r)
 }
 
 func main() {
-	fmt.Printf("%v\n", reverse("Chaotic"))
+	s := "sReveRs"
+	if len(os.Args) > 1 {
+		s = strings.Join(os.Args[1:], " ")
+	}
+	fmt.Printf("%v\n", reverse(s))
 }
