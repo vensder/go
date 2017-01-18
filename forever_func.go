6a145be0879bc81402035c685af9026ac72ebdf9
vensder <vensder@gmail.com>
2017-01-18 23:36:08 +0300

    forever_func.go

--- forever_func.go
+++ forever_func.go
@@ -6,20 +6,21 @@ import (
 	"strings"
 )
 
-var max_l int = 80
+var max_l int = 160
 
 func line(n int) {
-	symbol := "-"
+	symbol := "#"
 	fmt.Println(strings.Repeat(symbol, n))
 }
 
+// max_l * sin( (pi/2) * n/max_l)
 func sin_int(n int) int {
-	return int(math.Sin(float64(n/max_l) * (math.Pi / 2.0)) * float64(max_l))
+	return int(math.Ceil(float64(max_l) * math.Sin((math.Pi/2.0) * float64(n)/float64(max_l) )))
 }
 
 func main() {
 	for {
-		for i := 0; i < max_l; i++ {
+		for i := 1; i < max_l; i++ {
 			line(sin_int(i))
 		}
 		for i := max_l; i > 0; i-- {
